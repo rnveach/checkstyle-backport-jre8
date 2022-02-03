d57768b4fb18c83d5fd3c9e01c797c3823257870
rnveach <rveach02@gmail.com>
2022-02-02 20:13:19 -0500

    minor: added release to commit validation

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/CommitValidationTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/CommitValidationTest.java
@@ -75,7 +75,7 @@ public class CommitValidationTest {
     private static final String ISSUE_COMMIT_MESSAGE_REGEX_PATTERN = "^Issue #\\d+: .*$";
     private static final String PR_COMMIT_MESSAGE_REGEX_PATTERN = "^Pull #\\d+: .*$";
     private static final String OTHER_COMMIT_MESSAGE_REGEX_PATTERN =
-            "^(minor|config|infra|doc|spelling|dependency|supplemental): .*$";
+            "^(minor|config|infra|doc|spelling|dependency|supplemental|release): .*$";
 
     private static final String ACCEPTED_COMMIT_MESSAGE_REGEX_PATTERN =
               "(" + ISSUE_COMMIT_MESSAGE_REGEX_PATTERN + ")|"
